03d9ddb4ce21094da3abca6a3078dd6a3b2f9220
amang390 <100703463+amang390@users.noreply.github.com>
2025-01-19 16:01:34 +0530

    second_commit

--- app.py
+++ app.py
@@ -66,15 +66,15 @@ def scrape():
              'name': Name of the product, 
              'brand': Product brand,
              'category': Assign one of the value Hair, Skincare, Fragnances, Bath & Body, Makeup or Other,
-             'original price': Original price without any currency symbol,
-             'offer price': Offer price without any currency symbol,
-             'size in number': Size amount without the unit,
-             'size unit': Size unit such as gm, ml etc.,
+             'original_price': Original price without any currency symbol,
+             'offer_price': Offer price without any currency symbol,
+             'size_in_number': Size amount without the unit,
+             'size_unit': Size unit such as gm, ml etc.,
              'description': Only text, exlude images, rephrase if longer than 100 words to reduce it to 100 words,
              'ingredients': [List of ingredients],
-             'how to use': Bullet points of how to use,
+             'how_to_use': Bullet points of how to use,
              'rating': Return only rating value, exclude number of rating and reviews,
-             'Best before': Return best before time period of use since manufacture}
+             'best_before': Return best before time period of use since manufacture}
              </productinfo>"""
         
         prompt = prompt1 + doc1
@@ -82,6 +82,30 @@ def scrape():
         # Get the response from OpenAI
         ai_response = generate_response(prompt=prompt, model=GPT_MODEL)
         ai_response1 = json.loads(ai_response.choices[0].message.content)
+        
+        # Extract brand and name from AI response
+        brand = ai_response1.get('productinfo').get('brand', '').lower()
+        name = ai_response1.get('productinfo').get('name', '').lower()
+
+        # Load the CSV file
+        df = pd.read_csv('Skincare_final.csv')
+        
+        def calculate_similarity(row):
+            brand_similarity = fuzz.ratio(brand, row['Brand'].lower())
+            name_similarity = fuzz.ratio(name, row['Name'].lower())
+            return (brand_similarity + name_similarity) / 2  # Average similarity
+
+                # Apply similarity calculation
+        df['similarity'] = df.apply(calculate_similarity, axis=1)
+        
+        
+        if not df.empty:
+            # Return the best match
+            best_match = df.sort_values(by='similarity', ascending=False).reset_index(drop=True)[0]
+            ai_response1['matched_product'] = best_match
+        else:
+            ai_response1['matched_product'] = None
+
         # Extract the content
         return jsonify(ai_response1), 200
 
